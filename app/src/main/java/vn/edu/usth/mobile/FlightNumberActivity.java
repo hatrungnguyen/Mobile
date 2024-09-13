058327313ae01badb3005e2b5acce289ad459f0d
QuyTungDao <QuyTungDao@gmail.com>
2024-09-13 14:11:43 +0700

    be for cities and FlightNumberActivity

--- app/src/main/java/vn/edu/usth/mobile/FlightNumberActivity.java
+++ app/src/main/java/vn/edu/usth/mobile/FlightNumberActivity.java
@@ -1,6 +1,9 @@
 package vn.edu.usth.mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +11,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class FlightNumberActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flight_number);}
+        setContentView(R.layout.activity_flight_number);
+
+        MaterialButton citiesButton = findViewById(R.id.button_cities);
+        citiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+
+        ImageView homeIcon = findViewById(R.id.home_icon);
+        homeIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FlightNumberActivity.this, CitiesActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 
 }
