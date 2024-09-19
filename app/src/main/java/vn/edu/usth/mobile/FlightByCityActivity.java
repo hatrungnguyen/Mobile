df093601677dc769fab2a462b3c285139a2cae43
hoangviet <kinkinkin229@gmail.com>
2024-09-19 11:25:59 +0700

    flight by city layout

--- app/src/main/java/vn/edu/usth/mobile/FlightByCityActivity.java
+++ app/src/main/java/vn/edu/usth/mobile/FlightByCityActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class FlightByCitiesActivity extends AppCompatActivity {
+public class FlightByCityActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_flight_by_cities);
+        setContentView(R.layout.activity_flight_by_city);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
