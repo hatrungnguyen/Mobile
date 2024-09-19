d819e25fe57b17afdabcd05e5b5cb015c2df1003
NguyenTriAnhVu <nguyenvutri2206@gmail.com>
2024-09-19 13:55:30 +0700

    Update profile button

--- app/src/main/java/vn/edu/usth/mobile/ProfileActivity.java
+++ app/src/main/java/vn/edu/usth/mobile/ProfileActivity.java
@@ -1,7 +1,11 @@
 package vn.edu.usth.mobile;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,11 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+        
+        // Add OnClickListener to the profile icon
+        ImageView profileIcon = findViewById(R.id.profile_icon);
+        profileIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change this to navigate to the desired activity
+                Intent intent = new Intent(ProfileActivity.this, FlightByCityActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
